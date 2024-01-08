a4ee1acf3c1097584a81a8844f0df1d05083ba05
Darshan Chhetri <mitch4827@gmail.com>
2024-01-08 17:02:46 +0545

    password component

--- client/src/components/Recovery.jsx
+++ client/src/components/Recovery.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import profile from "../assets/profile.png";
+import styles from '../Styles/Username.module.css'
+import { Toaster } from 'react-hot-toast'
+import { useFormik } from "formik";
+import { passwordValidate } from '../helper/validate'
 
 const Recovery = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      password: 'admin@123'
+    },
+    validate: passwordValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async values => {
+      console.log(values);
+    }
+})
+
   return (
-    <div>
-      
+    <div className="container mx-auto">
+      <Toaster position='top-center' reverseOrder={false} ></Toaster>
+      <div className="flex justify-center items-center min-h-screen ">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className='text-5xl font-bold'>Password</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+              Explore More by Connecting With Us
+            </span>
+          </div>
+          <form className='py-1' onSubmit={formik.handleSubmit} action="">
+            <div className="profile flex justify-center py-4">
+              <img className={styles.profile_img} src={profile} alt="avatar" />
+            </div>
+            <div className="textbox flex flex-col items-center gap-6">
+              <input {...formik.getFieldProps('password')} className={styles.textbox} type="password" placeholder='Password' />
+              <button className={styles.btn} type='submit'>Login</button>
+            </div>
+            <div className="text-center py-4">
+              <span>Forgot password? <Link className='text-red-500' to="/recovery"> Recover Now</Link> </span>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Recovery
+
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -6,12 +6,36 @@ export async function usernameValidate(values){
     return errors;
 }
 
+// validate login page password
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values)
+    return errors;
+}
+
+// validate password
+function passwordVerify(errors ={} , values) {
+
+    const specialChars = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
+
+    if (!values.password){
+        errors.password = toast.error("Password is required.")
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Password cannot contain spaces.");
+    } else if(values.password.length < 4){
+        errors.password= toast.error('Password must contain at least 4 characters')
+    } else if(!specialChars.test(values.password)){
+        errors.password = toast.error('Password must contain at least one special character');
+    }
+    return errors
+}
+
+
 // validate username
 function usernameVerify(error = {}, values) {
   if (!values.username) {
     error.username = toast.error("Username is required.");
   } else if(values.username.includes(" ")){
-    error.username = toast.error("Username can not contain spaces.")
+    error.username = toast.error("Username cannot contain spaces.")
   }
   return error;
 }
